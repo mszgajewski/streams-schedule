4fa0d576fd840ebb5813d185063659b97c755272
mszgajewski <mszgajewski@gmail.com>
2022-02-17 21:46:55 +0100

    Initial Commit

---
+++ src/main/java/com/example/streamsschedule/controller/LiveStreamController.java
@@ -0,0 +1,11 @@
+package com.example.streamsschedule.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/streams")
+public class LiveStreamController {
+
+
+}
--- src/main/java/com/example/streamsschedule/model/ImmutableLiveStream.java
+++ src/main/java/com/example/streamsschedule/model/ImmutableLiveStream.java
@@ -3,73 +3,53 @@ package com.example.streamsschedule.model;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+public class ImmutableLiveStream {
 
-public class MutableLiveStream {
+    private final String id;
+    private final String title;
+    private final String description;
+    private final String url;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
 
-    private String id;
-    private String title;
-    private String description;
-    private String url;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-
-    public MutableLiveStream(){
-
+    public ImmutableLiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     public LocalDateTime getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
     public LocalDateTime getEndDate() {
         return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        MutableLiveStream that = (MutableLiveStream) o;
+        ImmutableLiveStream that = (ImmutableLiveStream) o;
         return id.equals(that.id) && title.equals(that.title) && description.equals(that.description) && url.equals(that.url) && startDate.equals(that.startDate) && endDate.equals(that.endDate);
     }
 
@@ -80,7 +60,7 @@ public class MutableLiveStream {
 
     @Override
     public String toString() {
-        return "MutableLiveStream{" +
+        return "ImmutableLiveStream{" +
                 "id='" + id + '\'' +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
